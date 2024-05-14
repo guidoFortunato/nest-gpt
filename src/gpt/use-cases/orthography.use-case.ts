08414a0969c2b45da92e2d4dff4893349745752a
Guido <46787485+guidoFortunato@users.noreply.github.com>
2024-05-14 19:41:34 -0300

    backend listo

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,3 +1,6 @@
+
+
+
 import OpenAI from "openai";
 
 interface Options{
@@ -10,20 +13,45 @@ export const orthographyCheckUseCase = async(openai: OpenAI, { prompt }: Options
         messages: [
             { 
                 role: "system", 
-                content: "Tu nombre es Daiana la asistente, siempre tenes que dar tu nombre al inicio de la respuesta y responder amablemente"
+                content: `
+                    Te serán proveídos textos en español con posibles errores ortograficos y gramaticales.
+                    Las palabras usadas deben de existir en el diccionario de la Real Academia Española.
+                    Debes responder en formato JSON.
+                    Tu tarea es corregirlos y retornar información soluciones.
+                    También debes de dar un porcentaje de acierto por el usuario.
+
+                    Si no hay errpres, debes de retornar un mensaje de felicitaciones.
+
+                    Ejemplo de salida:
+                    {
+                        userScore: number,
+                        errors: string[], // ['error -> solución']
+                        message: string, // Usa emojis y texto para felicitar al uusario
+                    }
+                `
             },
             { 
                 role: "user", 
                 content: prompt
             },
         ],
-        model: "gpt-3.5-turbo",
+        model: "gpt-4o-2024-05-13",
+        temperature: 0.3,
+        response_format: {
+            type: "json_object",
+        },
+        max_tokens: 150
       });
 
-    //   console.log({completion})
-    
-       return completion.choices[0]
+    //   console.log(completion.choices[0].message)
 
+    const jsonResp = JSON.parse(completion.choices[0].message.content)
+    
+    return jsonResp
+    // return completion.choices[0]
+
+
+    
 
 
   
